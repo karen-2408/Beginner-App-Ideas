e9b7eb890d9b9789763ff466897895819e4bf15c
karen-2408 <menghraj@ualberta.ca>
2025-10-28 14:14:38 -0600

    Second Page done

--- Instant Translation App/app/src/main/java/com/example/translatorapp/translator.java
+++ Instant Translation App/app/src/main/java/com/example/translatorapp/translator.java
@@ -11,25 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class translator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_translator);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button buttonNextActivity = findViewById(R.id.Ready);
-        buttonNextActivity.setOnClickListener(new View.OnClickListener() {
+        Button buttonback = findViewById(R.id.Back);
+        buttonback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create an Intent to start the SecondActivity
-                Intent intent = new Intent(MainActivity.this, translator.class);
+                // Create an Intent to start the MainActivity
+                Intent intent = new Intent(translator.this, MainActivity.class);
                 startActivity(intent); // Start the new activity
             }
         });
